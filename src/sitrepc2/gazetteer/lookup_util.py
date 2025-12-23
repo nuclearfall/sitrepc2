b131a7dc45093428587a300bf4e055a6fb0c59c5
nuclearfall <nuclearfall@protonmail.com>
2025-12-23 06:24:48 -0500

    file clean. Files no longer required

--- src/sitrepc2/gazetteer/lookup_util.py
+++
@@ -1,6 +0,0 @@
-from sitrepc2.config.paths import gazetteer_paths
-
-
-def get_lookup_files():
-	locale, region, *rest = gazetteer_paths()
-	return locale, region
--- src/sitrepc2/review/db.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-from sitrepc2.config.paths import get_lss_db_path
-
-def connect():
-    return sqlite3.connect(get_lss_db_path())
