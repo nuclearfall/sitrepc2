0a8e2eb039703b8ef50488fdf07c2a05d40ca5c6
nuclearfall <nuclearfall@protonmail.com>
2025-12-28 06:50:04 -0500

    added dom cmd

--- src/sitrepc2/cli/app.py
+++ src/sitrepc2/cli/app.py
@@ -10,6 +10,7 @@ from .lss_cmd import app as lss_app
 
 from .fetch_cmd import fetch_callback as fetch
 from .extract_cmd import extract_callback as extract
+from .dom_cmd import app as dom_app
 
 app = typer.Typer(help="sitrepc2 command line interface")
 
@@ -22,3 +23,4 @@ app.add_typer(lss_app, name="lss")
 # standalone verbs
 app.command("fetch")(fetch)
 app.command("extract")(extract)
+app.add_typer(dom_app, name="dom")
